b49308b270aa58b5dc5433ca97f25aa7035a1b15
Max Starman <max.starman@outlook.com>
2024-04-05 13:39:41 +0200

    Code cleanup

--- firebase-app/src/app/app-routing.module.ts
+++ firebase-app/src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {BlogComponent} from "./components/blog/blog.component";
 import {HomeComponent} from "./components/home/home.component";
-import {redirectLoggedInTo} from "@angular/fire/auth-guard";
 import {AuthGuard} from "./auth.guard";
-
-const redirectLoggedInToBlog = () => redirectLoggedInTo(['blog']);
 
 const routes: Routes = [
 	{path: '', redirectTo: 'home', pathMatch: 'full'},
@@ -13,7 +10,6 @@ const routes: Routes = [
 	{path: 'blog', component: BlogComponent, canActivate: [AuthGuard]}
 ];
 
-// canActivate: [AuthGuard], data: {redirectLoggedInToBlog}
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
 	exports: [RouterModule]
